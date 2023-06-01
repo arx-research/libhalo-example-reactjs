eadd44b191d72e1412fce257ccfa858ea9048bad
Michał Leszczyński <ml@icedev.pl>
2023-06-01 12:15:25 +0200

    fix webpack for the latest version

---
+++ config-overrides.js
@@ -0,0 +1,10 @@
+const webpack = require('webpack');
+
+module.exports = function override(config, env) {
+    config.resolve.fallback = {
+        "crypto": require.resolve("crypto-browserify"),
+        "stream": require.resolve("stream-browserify")
+    };
+
+    return config;
+};
